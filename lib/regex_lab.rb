d1dad9d71eb160c4cb844fe2c66aba7f3e953df4
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-04-26 02:41:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-word.scan(/\^b[aeiouAEIOU]/) ?  true : false
+word.scan(/\A[aeiouAEIOU]i/) ?  true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
