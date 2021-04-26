260dfbb994797299702d1899043b947e4f6516a1
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-04-26 04:01:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,5 +16,5 @@ text.match(/^[A-Z]\W/) ? true : false
 end
 
 def valid_phone_number?(phone)
-phone.match(/\d{3}()\d{3}()\d{4}/) ? true : false
+phone.match(/\d{3}+\d{3}+\d{4}/) ? true : false
 end
